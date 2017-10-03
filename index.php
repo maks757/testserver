5a435030c20f10c3318718e530f559e1f786bb91
max <maks757q@gmail.com>
2017-10-03 18:17:11 +0300

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Name: Cherednyk Maxim
+ * Phone: +380639960375
+ * Email: maks757q@gmail.com
+ * Date: 03.10.17
+ * Time: 18:16
+ */
+phpinfo();
